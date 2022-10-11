37b9aaf54fc00791aef88468146b28c745d609eb
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-10-11 01:00:05 -0500

    Time: 33 ms (25.52%), Space: 38.3 MB (100.00%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.java
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.java
@@ -1,10 +1,10 @@
 class Solution {
     public int countQuadruplets(int[] nums) {
-        int ans = 0;
-        for (int i = 0; i< nums.length - 3; i++) {
-            for (int j = i+1; j< nums.length - 2;j ++){
-                for (int k = j+1; k < nums.length - 1; k++) {
-                    for (int l = k + 1; l < nums.length; l++) {
+        int ans = 0, len = nums.length;
+        for (int i = 0; i< len - 3; i++) {
+            for (int j = i+1; j< len - 2;j ++){
+                for (int k = j+1; k < len - 1; k++) {
+                    for (int l = k + 1; l < len; l++) {
                         if (nums[i] + nums[j] + nums[k] == nums[l]) ans++;
                     }
                 }
