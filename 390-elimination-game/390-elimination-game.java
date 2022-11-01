d5543971137697a3e90336c9df5e90299cb51790
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-10-31 19:28:20 -0500

    Time: 5 ms (72.96%), Space: 41.7 MB (74.00%) - LeetHub

--- 390-elimination-game/390-elimination-game.java
+++ 390-elimination-game/390-elimination-game.java
@@ -4,7 +4,7 @@ class Solution {
         // return 
         int it = 0, ans = 1;
         boolean left = true;
-        while (n > 1) {
+        while (n != 1) {
             if ((n&1) == 1 || (it&1) == 0) {
                 ans += (1 << it);
             }
