487ae6053cfedc873d1569eb6eda2ee0840a388e
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-10-31 21:02:52 -0500

    Time: 1 ms (93.58%), Space: 43.9 MB (44.29%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -4,13 +4,13 @@ class Solution {
         return nums[0];
     }
     
-    private int calc(int[] nums, int end) {
-        if(end <= 1) return 0;
+    private void calc(int[] nums, int end) {
+        if(end <= 1) return;
         end = end / 2;
         for(int i = 0; i < end; i++) {
             if ((i&1) == 0) nums[i] = Math.min(nums[2*i], nums[2*i+1]);
             else nums[i] = Math.max(nums[2*i], nums[2*i+1]);
         }
-        return calc(nums, end);
+        calc(nums, end);
     }
 }
