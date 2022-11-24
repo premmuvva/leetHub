e906a82163ac183fcc46c82dbac5eda444f074e0
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-11-24 00:22:50 -0600

    Time: 132 ms (82.50%), Space: 42.4 MB (32.21%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
+        char[] str = word.toCharArray();
         for(int i = 0; i< board.length; i++) 
             for (int j = 0; j<board[0].length; j++)
-                if (board[i][j] == word.charAt(0))
-                    if(solve(board, visited, word, 0, i, j))
+                if (board[i][j] == str[0])
+                    if(solve(board, visited, str, 0, i, j))
                         return true;
         return false;
     }
     
-    private boolean solve(char[][] board, boolean[][] visited, String word, int st, int i, int j) {
-        if (word.length() == st) return true;
+    private boolean solve(char[][] board, boolean[][] visited, char[] word, int st, int i, int j) {
+        if (word.length == st) return true;
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) return false;
         visited[i][j] = true;
-        if (board[i][j] == word.charAt(st)) {
+        if (board[i][j] == word[st]) {
             if (solve(board, visited, word, st+1, i, j+1) || solve(board, visited, word, st+1, i+1, j) 
                 || solve(board, visited, word, st+1, i-1, j) || solve(board, visited, word, st+1, i, j-1)) {
                 return true;
