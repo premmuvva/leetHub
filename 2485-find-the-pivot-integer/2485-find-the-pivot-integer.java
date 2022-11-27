5a4f2718c88332fc25e949570609c752760168ce
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-11-26 23:23:30 -0600

    Time: 1 ms (88.89%), Space: 41.2 MB (33.33%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,7 +1,7 @@
 class Solution {
     public int pivotInteger(int n) {
         int tot = (n + 1) * n / 2;
-        int temp = (-2+ (int) Math.sqrt(4 + 4 * tot - 4 ))/2;
-        return temp * (temp + 1) == tot - temp - 1 ? temp + 1 : -1;
+        int temp = ((int) Math.sqrt(tot));
+        return temp * temp == tot ? temp : -1;
     }
 }
