bfb68cc43aaa212abd32d7f861d466dfea976742
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2022-12-03 22:31:12 -0600

    Time: 1 ms (100.00%), Space: 40.9 MB (100.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        for (int i = 1; i < sentence.length() - 1; i++) {
+        int n = sentence.length() - 1;
+        for (int i = 1; i < n; i++) {
             if (sentence.charAt(i) == ' ') {
                 if (sentence.charAt(i - 1) != sentence.charAt(i + 1)) 
                     return false;
             }
         }
-        return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
+        return sentence.charAt(0) == sentence.charAt(n);
     }
 }
