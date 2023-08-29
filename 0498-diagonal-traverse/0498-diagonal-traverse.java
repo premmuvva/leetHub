5f66b0d921aa47cc871620193ea9600377bf1240
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-08-29 00:32:10 -0500

    Time: 264 ms (5.05%), Space: 45.6 MB (99.55%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int [] ans = new int[mat.length * mat[0].length];
-        int co = 0;
-        for (int i = 0; i < 2 * Math.max(mat.length, mat[0].length); i++) {
-            int temp1 = Math.min(i, mat[0].length - 1);
+        int co = 0, temp1, temp2 = 2 * Math.max(mat.length, mat[0].length);
+        for (int i = 0; i < temp2 ; i++) {
+            temp1 = Math.min(i, mat[0].length - 1);
             for (int j = 0; j <= temp1; j++) {
                 if (mat.length > i-j) {
                     ans[co++] = mat[i-j][j];
