e33340e1ae5f05d9398e3885e9449f016624a412
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-09-21 00:46:07 -0500

    Time: 23 ms (76.84%), Space: 89.6 MB (46.95%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,27 @@
+static const auto _____ = []()
+{
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        return nullptr;
+ }();
+class Solution {
+public:
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=(nums1.size()+nums2.size())/2,a1=0,b1=0;
+        nums1.push_back(INT_MAX);
+        nums2.push_back(INT_MAX);
+        if(nums1.size()+nums2.size()&1){
+            while((a1+b1)!=n){
+                if(nums1[a1]>nums2[b1]) b1++;
+                else a1++;
+            }
+            return nums1[a1]>nums2[b1]? nums2[b1] : nums1[a1];
+        }
+        while((a1+b1)!=n-1){
+            if(nums1[a1]>nums2[b1]) b1++;
+            else a1++;
+        }
+        return ((nums1[a1]>nums2[b1]? nums2[b1++] : nums1[a1++]) + (nums1[a1]>nums2[b1]? nums2[b1] : nums1[a1]))/2.0;
+    }
+};
