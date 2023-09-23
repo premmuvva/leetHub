cc9fbda9d05e90434584f157204885cc75c452ed
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-09-23 00:53:23 -0500

    Time: 41 ms (63.38%), Space: 44.1 MB (69.73%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -16,7 +16,7 @@ class Solution {
     private int dp[][] = new int[105][105];
     public int minimumEffortPath(int[][] heights) {
         int m = heights.length, n = heights[0].length;
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(-a[0], -b[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
         for (int i = 0; i < m; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
@@ -28,14 +28,14 @@ class Solution {
         while(!pq.isEmpty()) {
             int cur[] = pq.poll();
             int i = cur[1], j = cur[2];
-            if(i == m -1 && j == n - 1) return -cur[0];
+            if(i == m -1 && j == n - 1) return cur[0];
             for (int te = 0; te < 4; te++) {
                 int ni = i + x[te], nj = j + y[te];
                 if(ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
                 int temp = Math.max( dp[i][j], Math.abs(heights[i][j] - heights[ni][nj]));
                 if (temp < dp[ni][nj]) {
                     dp[ni][nj] = temp;
-                    pq.add(new int[]{-temp, ni, nj});
+                    pq.add(new int[]{temp, ni, nj});
                 }
             }
         }
