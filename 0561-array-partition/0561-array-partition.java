3416f88277908054a9f2df0c8b0a7b731fe00151
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-09-25 00:25:34 -0500

    Time: 12 ms (95.27%), Space: 45.3 MB (21.94%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,10 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length/2, ans = 0;
+        int n = nums.length, ans = 0;
         
-        for(int i = 0; i<n; i++) {
-            ans += nums[2 * i];
+        for(int i = 0; i<n; i+=2) {
+            ans += nums[i];
         }
         return ans;
     }
