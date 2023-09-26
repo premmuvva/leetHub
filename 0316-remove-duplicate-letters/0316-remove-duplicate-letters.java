68d6dfb67483244871b7c14bcfaff2fcac551e9b
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-09-26 17:43:51 -0500

    Time: 6 ms (15.30%), Space: 41 MB (78.55%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,42 @@
+public class Solution {
+
+    public String removeDuplicateLetters(String s) {
+        if (s == null || s.length() <= 1) return s;
+
+        Map<Character, Integer> lastPosMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastPosMap.put(s.charAt(i), i);
+        }
+
+        char[] result = new char[lastPosMap.size()];
+        int begin = 0, end = findMinLastPos(lastPosMap);
+
+        for (int i = 0; i < result.length; i++) {
+            char minChar = 'z' + 1;
+            for (int k = begin; k <= end; k++) {
+                if (lastPosMap.containsKey(s.charAt(k)) && s.charAt(k) < minChar) {
+                    minChar = s.charAt(k);
+                    begin = k+1;
+                }
+            }
+
+            result[i] = minChar;
+            if (i == result.length-1) break;
+
+            lastPosMap.remove(minChar);
+            if (s.charAt(end) == minChar) end = findMinLastPos(lastPosMap);
+        }
+
+        return new String(result);
+    }
+
+    private int findMinLastPos(Map<Character, Integer> lastPosMap) {
+        if (lastPosMap == null || lastPosMap.isEmpty()) return -1;
+        int minLastPos = Integer.MAX_VALUE;
+        for (int lastPos : lastPosMap.values()) {
+             minLastPos = Math.min(minLastPos, lastPos);
+        }
+        return minLastPos;
+    }
+
+}
