0b657f65e48f2810ede86040e9c38a408768db6d
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-01 01:00:04 -0500

    Time: 5 ms (53.94%), Space: 44 MB (47.58%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int m = grid.length, n = grid[0].length, ans = 0;
+        for(int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    ans += solve(grid, i, j, m, n);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    private int solve(int[][] grid, int i, int j, int m, int n) {
+        int ans = 0;
+        if (i == 0) ans++;
+        if (i == m - 1) ans++;
+        if (j == 0) ans++;
+        if (j == n - 1) ans++;
+        if (i > 0 && grid[i-1][j] != 1) ans++;
+        if (i < m - 1 && grid[i+1][j] != 1) ans++;
+        if (j > 0 && grid[i][j-1] != 1) ans++;
+        if (j < n -1 && grid[i][j+1] != 1) ans++;
+        return ans;
+    }
+}
