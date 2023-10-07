c8ebf7f0685b6d0db4fa37dd19e97df27277c6b0
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-06 22:50:15 -0500

    Time: 2 ms (57.85%), Space: 55.1 MB (86.87%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -2,12 +2,11 @@ class Solution {
     public boolean isMonotonic(int[] nums) {
         if (nums.length <= 2) return true;
         boolean temp = false, flag = false;
-        int i = 1;
-        while(i<nums.length && nums[i-1] == nums[i]) i++;
-        if(i== nums.length) return true;
+        int i = 1, n = nums.length;
+        while(i<n && nums[i-1] == nums[i]) i++;
+        if(i== n) return true;
         temp = nums[i-1] > nums[i];
-        // System.out.println(true^false);
-        for(; i<nums.length; i++) {
+        for(; i<n; i++) {
             if(nums[i-1] == nums[i]) continue;
             if(temp ^ (nums[i-1] > nums[i])) return false;
         }
