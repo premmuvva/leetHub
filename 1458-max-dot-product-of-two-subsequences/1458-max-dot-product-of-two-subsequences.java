80021dafacceaf8663feac75acf666847972f202
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-08 03:42:15 -0500

    Time: 11 ms (58.33%), Space: 43.7 MB (26.39%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -2,10 +2,6 @@ class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
         int dp[][] = new int[nums1.length + 2][nums2.length + 2];
         int ans = Integer.MIN_VALUE;
-        // System.out.printf("     ");
-        // for(int i = 0; i < nums2.length-1; i++) 
-        //     System.out.printf("%d  ", nums2[i]);
-        // System.out.println("");
         for(int i = 1; i <= nums1.length; i++) {
             for(int j = 1; j <= nums2.length; j++) {
                 int temp = nums1[i-1] * nums2[j-1];
@@ -13,17 +9,6 @@ class Solution {
                 ans = Math.max(ans, dp[i][j]);
             }
         }
-        
-        // for(int i = 1; i <= nums1.length; i++) {
-        //     System.out.printf("%d  | ", nums1[i-1]);
-        //     for(int j = 1; j <= nums2.length; j++) {
-        //         System.out.printf("%d ", dp[i][j]);
-        //     }
-        //     System.out.println("");
-        // }
-        // System.out.println("");
-        // System.out.println("");
-        // System.out.println("");
         return ans;
     }
 }
