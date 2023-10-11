189f1758b436c127ab2df56b7ff64c0df0a9d79b
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-11 00:24:59 -0500

    Time: 2 ms (99.77%), Space: 43.7 MB (84.78%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder ans = new StringBuilder();
+        int si = s.length(), temp = 0;
+        if(numRows == 1) return s;
+        boolean flag;
+        for (int i = numRows; i > 0; i--) {
+            temp = numRows - i;
+            flag = false;
+            while(temp < si) {
+                ans.append(s.charAt(temp));
+                if (flag && (numRows - i) != 0) temp += (2 * (numRows - i));
+                else if(i != 1) temp+= (2 * i - 2);
+                else temp += (2 * numRows - 2);
+                // System.out.printf("%d ",temp);
+                flag = !flag;
+            }
+            // System.out.println();
+        }
+        return ans.toString();
+    }
+}
