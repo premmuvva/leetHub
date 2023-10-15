62a6aeb69aded8a31b062e790ec88c4bc65142c3
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-15 18:48:11 -0500

    Time: 11 ms (100.00%), Space: 71.1 MB (100.00%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -19,38 +19,6 @@ class Solution {
                 temp = (temp * grid[i][j]) % mod;
             }
         }
-        // int pro = 1, mod = 12345;
-        // for (int i = 0; i<m; i++) {
-        //     for (int j = 0; j <n ;j++) {
-        //         pro = (pro * grid[i][j]) % mod;
-        //     }
-        // }
-        // int ans[][] = new int[m][n];
-        // BigInteger bmod = new BigInteger(String.valueOf(mod)), bpro;
-        // for (int i = 0; i<m; i++) {
-        //     for (int j = 0; j <n ;j++) {
-        //         int div = GCD(grid[i][j], mod);
-        //         bpro = new BigInteger(String.valueOf(grid[i][j]/div));
-        //         bmod = new BigInteger(String.valueOf(mod/div));
-        //         ans[i][j] = (((bpro.modInverse(bmod)).intValue() % mod) * pro) % mod;
-        //         System.out.printf("%d %d %d \n", bpro, bmod, ans[i][j]);
-        //     }
-        // }
         return ans;
     }
-    
-    private int GCD(int x, int y) 
-    { 
-        // Everything is divisible by 0 
-        if (x == 0) 
-            return y; 
-        if (y == 0) 
-            return x; 
-        if (x == y) 
-            return x; 
-  
-        if (x > y) 
-            return GCD(x - y, y); 
-        return GCD(x, y - x); 
-    } 
 }
