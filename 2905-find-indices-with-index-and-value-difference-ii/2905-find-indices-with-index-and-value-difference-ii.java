0c9653fc2f075d68d0b261ec388610c65f68fce9
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-15 05:05:02 -0500

    Time: 1 ms (100.00%), Space: 60.1 MB (50.00%) - LeetHub

--- 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.java
+++ 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.java
@@ -1,18 +1,8 @@
 class Solution {
     public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
         int i = 0, n = nums.length, min = nums[0], max = nums[0], mi = 0, mai = 0;
-        int ii = 1;
-        if(n == 1) {
-            if(valueDifference == 0 && indexDifference == 0) return new int[]{0, 0};
-            return new int[]{-1,-1};
-        }
-        for(i=indexDifference; i<n && ii<n; i++, ii++) {
-            if(Math.abs(nums[i] - min) >= valueDifference) {
-                return new int[]{mi, i};
-            }
-            if(Math.abs(nums[i] - max) >= valueDifference) {
-                return new int[]{mai, i};
-            }
+        int ii = 0;
+        for(i=indexDifference; i<n; i++, ii++) {
             
             if(min > nums[ii]) {
                 min = nums[ii];
@@ -24,14 +14,13 @@ class Solution {
                 mai = ii;
             } 
             
-        }
-        if (i<n) {
             if(Math.abs(nums[i] - min) >= valueDifference) {
                 return new int[]{mi, i};
             }
             if(Math.abs(nums[i] - max) >= valueDifference) {
                 return new int[]{mai, i};
             }
+            
         }
         return new int[]{-1, -1};
     }
