ccc11f03b8cd14d5d307e65f8dff6389d186ca31
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-15 02:49:54 -0500

    Time: 2 ms (100.00%), Space: 41 MB (100.00%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        String ans = "23242342424233423231231213213123131231231231232132131321312131231213231232313213211322131223132112312", temp;
+        String ans, temp;
         int c = 0, n = s.length(), i = 0, pr = Integer.MAX_VALUE;
         List<Integer> dp = new ArrayList();
         while(i < n) {
@@ -20,24 +20,7 @@ class Solution {
                     ans = temp;
                 }
             }
-            // System.out.println
         }
-        
-//         if(c!=k) return "";
-//         pr = s.indexOf('1', 0);
-//         ans = s.substring(pr, i);
-//         for(;i<n;i++) {
-//             if(s.charAt(i) == '1') {
-//                 pr = s.indexOf('1', pr+1);
-//                 temp = s.substring(pr, i + 1);
-//                 if(temp.length() <= ans.length() && temp.compareTo(ans) < 0) {
-//                     ans = temp;
-//                 }
-                
-//             }
-//         }
-        // pr = ans.indexOf('1', 0);
-        // if(pr == -1) return "";
         return ans;
     }
 }
