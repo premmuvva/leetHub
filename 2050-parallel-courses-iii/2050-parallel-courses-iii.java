0a87a82391f1d2e99561b623f323443cc798d217
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-18 17:50:15 -0500

    Time: 28 ms (60.44%), Space: 77.3 MB (11.56%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,23 +1,23 @@
 class Solution {
-    int[] sol;
+    
     public int minimumTime(int n, int[][] relations, int[] time) {
-        sol = new int[n+1];
+        int[] sol = new int[n+1];
         int ans = Integer.MIN_VALUE;
         Map<Integer, List<Integer>> dp = new HashMap();
         for (int[] i: relations) {
             dp.computeIfAbsent(i[1], v -> new ArrayList()).add(i[0]);
         }
         for(int i = 1; i <= n; i++) {
-            ans = Math.max(cal(i, dp, time), ans);
+            ans = Math.max(cal(i, dp, time, sol), ans);
         }
         return ans;
     }
     
-    private int cal(int n, Map<Integer, List<Integer>> dp, int[] time) {
+    private int cal(int n, Map<Integer, List<Integer>> dp, int[] time, int[] sol) {
         if (sol[n] != 0) return sol[n];
         int ans = Integer.MIN_VALUE, nt = time[n-1];
         for(int i : dp.getOrDefault(n, new ArrayList<Integer>())) {
-            ans = Math.max(cal(i, dp, time) + nt, ans);
+            ans = Math.max(cal(i, dp, time, sol) + nt, ans);
         }
         sol[n] = Math.max(nt, ans);
         return sol[n];
