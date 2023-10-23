6ba02a8644137af3219af257490f0fac2493c8fa
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-23 16:07:40 -0500

    Time: 1 ms (76.18%), Space: 39.9 MB (14.52%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -3,10 +3,11 @@ class Solution {
         // if (n == 1) return true;
         // n /= 4;
         if (n <= 0) return false;
-        while (n > 0) {
-            if ((n&3) == 2 || (n&3) == 3 || ((n&3) == 1 && n/4 != 0)) return false;
-            n = n >> 2;
-        }
-        return true;
+        return Math.pow(4, (int)(Math.log(n)/Math.log(4))) == n;
+        // while (n > 0) {
+        //     if ((n&3) == 2 || (n&3) == 3 || ((n&3) == 1 && n/4 != 0)) return false;
+        //     n = n >> 2;
+        // }
+        // return true;
     }
 }
