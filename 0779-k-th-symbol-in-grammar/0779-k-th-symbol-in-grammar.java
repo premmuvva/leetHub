1d55984c39acd08ddb99b15a4948435fbd2e2dba
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-24 20:13:48 -0500

    Time: 0 ms (100.00%), Space: 38.7 MB (90.11%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -3,8 +3,7 @@ class Solution {
         k--; n--;
         boolean flag = true;
         while(n>=0) {
-            int temp1 = k%2;
-            if(temp1 == 1) flag = !flag;
+            if(k%2 == 1) flag = !flag;
             n = n - 1;
             k/=2;
         }
