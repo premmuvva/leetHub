ce4b9ee0578d2096e66d4f6f44cc1f5d18e42c70
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-24 20:13:15 -0500

    Time: 0 ms (100.00%), Space: 39.3 MB (26.34%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -7,9 +7,7 @@ class Solution {
             if(temp1 == 1) flag = !flag;
             n = n - 1;
             k/=2;
-            // System.out.printf("%d %s \n", k, flag);
         }
-        // System.out.println();
         return flag ? 0 : 1;
     }
 }
