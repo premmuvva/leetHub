d1f0059a56e3a6b93067bf78642bcdb349b86800
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-27 00:14:12 -0500

    Time: 1684 ms (6.11%), Space: 43.9 MB (46.91%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        Map<Character, List<Integer>> hm = new HashMap();
+        int n = s.length(), nn = -1, m, ii, j, jj;
+        String ans = s.substring(0, 1);
+        char ch;
+        for(int i = 0; i < n; i++) {
+            ch = s.charAt(i);
+            if(!hm.containsKey(ch)) {
+                hm.put(ch, new ArrayList());
+            }
+            hm.get(ch).add(i);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            ch = s.charAt(i);
+            m = hm.get(ch).size();
+            while(m-- != 0) {
+                ii = i;
+                jj = hm.get(ch).get(m);
+                for(j = jj; ii <= j && s.charAt(ii) == s.charAt(j); ii++, j--) {
+                    // if(s.charAt(ii) != s.charAt(j)) {
+                    //     break;
+                    // }
+                }
+                // System.out.printf("%d %d %d %c\n", jj, ii, i, ch);
+                if(ii > j) {
+                    if(jj - i > nn) {
+                        // System.out.printf("%d %d %d \n\n", jj, nn, i);
+                        ans = s.substring(i, jj + 1);
+                        nn = jj - i;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
