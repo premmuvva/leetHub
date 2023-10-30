9cf8e6145d1a53ecc3f38641ae73e4cc90a4602f
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-30 12:46:23 -0500

    Time: 6 ms (64.52%), Space: 43.7 MB (68.07%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -3,7 +3,7 @@ class Solution {
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> (a[0]-b[0] == 0) ? (a[1]-b[1]) : a[0]-b[0]);
         int n = arr.length, i;
         for(int ii: arr) 
-            pq.add(new int[]{count(ii), ii});
+            pq.add(new int[]{Integer.bitCount(ii), ii});
         i = 0;
         while(pq.size() > 0) {
             arr[i++] = pq.poll()[1];
