370c2d8185aabe5ffbdcd571452f37fe079efeec
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-10-30 13:13:37 -0500

    Time: 7 ms (66.15%), Space: 43.7 MB (36.48%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        // Arrays.sort(nums);
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
         PriorityQueue<int[]> pq1 = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
         int[] ans = new int[k];
@@ -9,12 +8,9 @@ class Solution {
             pq.add(new int[]{nums[i], i});
             if(pq.size() > k) pq.poll();
         }
-        // System.out.println(Arrays.deepToString(pq.toArray()));
         while(pq.size() != 0) 
             pq1.add(pq.poll());
         int i = 0;
-        // System.out.println(Arrays.deepToString(pq1.toArray()));
-        // System.out.println();
         while(pq1.size() != 0) {
             ans[i++] = pq1.poll()[0];
         }
