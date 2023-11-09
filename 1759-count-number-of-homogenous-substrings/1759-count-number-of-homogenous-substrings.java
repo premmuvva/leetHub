c7e5b33b1baaf9484e158d0b82954ee0ae82b5f7
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-11-09 10:29:54 -0600

    Time: 10 ms (65.20%), Space: 44 MB (77.60%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -4,22 +4,15 @@ class Solution {
         long ans = 0L, cou = 1;
         char ch;
         for(int i = 1; i<n; i++) {
-            if(s.charAt(i-1) == s.charAt(i)) {
+            if (s.charAt(i-1) == s.charAt(i)) {
                 cou++;
             } else {
                 ch = s.charAt(i-1);
-                ans += ((cou * (cou + 1))/2 % 1000000007);
+                ans = ((ans + ((cou * (cou + 1L))/2) % 1000000007L) % 1000000007L);
                 cou = 1;
             }
         }
-        // hm.get(s.charAt(n-1)).merge(cou, 1, Integer::sum);
         ans += ((cou * (cou + 1))/2);
-        
-//         for(Map<Integer, Integer> hh: hm.values()) {
-//             for(int ii: hh.values()) {
-//                 ans += ((ii * (ii + 1))/2);
-//             }
-//         }
         return (int) (ans % 1000000007);
     }
 }
