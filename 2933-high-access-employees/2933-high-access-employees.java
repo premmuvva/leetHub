af55d84538a7defae8947f38c59125a84339f750
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2023-11-11 23:41:52 -0600

    Time: 9 ms (100.00%), Space: 44.6 MB (50.00%) - LeetHub

---
+++ 2933-high-access-employees/2933-high-access-employees.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> findHighAccessEmployees(List<List<String>> access_times) {
+        Map<String, List<Integer>> hm = new HashMap();
+        for (List<String> en: access_times) {
+            if(!hm.containsKey(en.get(0))) hm.put(en.get(0), new ArrayList());
+            int n = Integer.parseInt(en.get(1));
+            hm.get(en.get(0)).add((n/100)*60 + (n%100));
+        }
+        List<String> ans = new ArrayList();
+        int temp[] = new int[100], i;
+        for(Map.Entry<String, List<Integer>> en: hm.entrySet()) {
+            i = 0;
+            List<Integer> val = en.getValue();
+            Collections.sort(val);
+            if(val.size() < 3) continue;
+            for(int j = 2; j<val.size(); j++) {
+                if(val.get(j) - val.get(j-2) < 60) {
+                    ans.add(en.getKey());
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
