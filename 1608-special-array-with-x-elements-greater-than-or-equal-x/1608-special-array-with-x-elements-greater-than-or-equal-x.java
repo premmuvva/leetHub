15e0d0cd99be64ac466951e646a25b1cbcb3b280
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-08-03 14:33:51 -0500

    Time: 1 ms (79.49%), Space: 41.5 MB (15.03%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,19 +1,14 @@
 class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        // if (n < 3 && nums[0] == 1) {
-        //     if (n == 1) return 1;
-        //     return nums[1] == 1? -1 : 1;
-        // }
-        int temp;
-        for (int i = 1; i <= n; i++) {
+        int n = nums.length, temp;
+        for (int i = 1; i < n; i++) {
             // System.out.printf("%d %d\n", i, nums[n-i]);
-            temp = n - i >= 1 ? nums[n - i - 1] : Integer.MIN_VALUE;
-            if (i <= nums[n - i] && i > temp) {
+            // temp = n - i >= 1 ? nums[n - i - 1] : Integer.MIN_VALUE;
+            if (i <= nums[n - i] && i > nums[n - i - 1]) {
                 return i;
             }
         }
-        return -1;
+        return n <= nums[0] ? n : -1;
     }
 }
