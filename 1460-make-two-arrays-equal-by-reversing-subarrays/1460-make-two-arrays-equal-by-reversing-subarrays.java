5284d4209a5a36e36d9781586cf7aefa538ed106
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-08-02 21:42:53 -0500

    Time: 3 ms (85.96%), Space: 44.2 MB (56.26%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        int i, nt = target.length, na = arr.length;
-        Map<Integer, Integer> hm = new HashMap();
-        for (i = 0; i<nt; i++) {
-            hm.put(target[i], hm.getOrDefault(target[i], 0) + 1);
+        int i, nt = target.length;
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        
+        for (i = 0; i < nt; i++) {
+            if (target[i] != arr[i]) return false;
         }
-        try {
-            for (i = 0; i<na; i++) {
-                hm.put(arr[i], hm.get(arr[i]) - 1);
-                if(hm.get(arr[i]) < 0) {
-                    return false;
-                }
-            }
-        } catch (Exception e) {
-            return false;
-        }
+        
+        // Map<Integer, Integer> hm = new HashMap();
+        // for (i = 0; i<nt; i++) {
+        //     hm.put(target[i], hm.getOrDefault(target[i], 0) + 1);
+        // }
+        // try {
+        //     for (i = 0; i<nt; i++) {
+        //         hm.put(arr[i], hm.get(arr[i]) - 1);
+        //         if(hm.get(arr[i]) < 0) {
+        //             return false;
+        //         }
+        //     }
+        // } catch (Exception e) {
+        //     return false;
+        // }
         return true;
     }
 }
