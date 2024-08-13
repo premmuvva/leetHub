2f554496f45043129c7d06e1ce50375ad4c75d6e
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-08-12 20:47:09 -0500

    Time: 13 ms (82.42%), Space: 46.9 MB (46.55%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -7,8 +7,15 @@ class KthLargest {
         this.nums = nums;
         this.pq = new PriorityQueue<Integer>();
         this.k = k;
-        for (int i : nums) {
-            pq.add(i);
+        int n = nums.length;
+        for (int i = 0; i < Math.min(k, n); i++) {
+            pq.add(nums[i]);
+        }
+        for (int i = k; i < n; i++) {
+            if (nums[i] > pq.peek()){
+                pq.add(nums[i]);
+                pq.poll();
+            }
         }
         while(pq.size() > k) {
             pq.poll();
