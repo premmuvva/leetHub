b8b6a2c22852895eda9ef27669200f3300839254
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-08-29 03:33:47 -0500

    Time: 4 ms (82.28%), Space: 54 MB (15.28%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int i = 1, n = values.length, a = Integer.MIN_VALUE, b = Integer.MIN_VALUE;
+        int arr[] = new int[n];
+        arr[0] = values[0];
+        for(i=1; i< n; i++) {
+             arr[i] = Math.max(arr[i-1], i + values[i]);
+        }
+        values[n-1] = values[n-1] - n + 1;
+        for(i=n-2; i>=0; i--) {
+             values[i] = Math.max(values[i+1], values[i] - i);
+        }
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(values));
+        for (i =0; i<n-1; i++) {
+            a = Math.max(a, arr[i] + values[i+1]);
+        }
+        return a;
+    }
+}
