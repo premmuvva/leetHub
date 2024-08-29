b525d5afdce9d17e3650d7b3072655a9496962c6
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-08-29 03:47:55 -0500

    Time: 50 ms (36.83%), Space: 44.5 MB (35.58%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int nthUglyNumber(int n) {
         TreeSet<Long> pq = new TreeSet();
         pq.add(1L);
-        // int arr [] = new int[]{2, 3, 5};
         long i = 0, num = 0;
         while (n > 0) {
             num = pq.pollFirst();
@@ -11,7 +10,6 @@ class Solution {
             pq.add(num * 5);
             n--;
         }
-        // System.out.println(pq);
         return (int) num;
     }
 }
