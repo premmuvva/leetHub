15f5e92ed30bc5d9b620f1a7eb0cb673c12b7afb
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-02 02:01:10 -0500

    Time: 11 ms (28.01%), Space: 45.3 MB (56.03%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -5,7 +5,8 @@ class Solution {
         int mo = Integer.parseInt(sp[1]), yy = Integer.parseInt(sp[0]);
         int[] prefix = new int[]{0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
         ans += (prefix[mo] + Integer.parseInt(sp[2]));
-        if (yy % 4 == 0 && (yy %100 != 0 || yy % 400 == 0) && mo > 2) ans ++;
+        if (mo <= 2) return ans;
+        if (yy % 4 == 0 && (yy %100 != 0 || yy % 400 == 0)) return ans + 1;
         return ans;
     }
 }
