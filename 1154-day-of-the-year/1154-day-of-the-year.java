2cec043a57e9d04c69ced6921d0e70656894ca72
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-02 02:03:40 -0500

    Time: 10 ms (49.08%), Space: 45.4 MB (37.02%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -2,10 +2,11 @@ class Solution {
     public int dayOfYear(String date) {
         int ans = 0;
         String sp[] = date.split("-");
-        int mo = Integer.parseInt(sp[1]), yy = Integer.parseInt(sp[0]);
+        int mo = Integer.parseInt(sp[1]);
         int[] prefix = new int[]{0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
         ans += (prefix[mo] + Integer.parseInt(sp[2]));
         if (mo <= 2) return ans;
+        int yy = Integer.parseInt(sp[0]);
         if (yy % 4 == 0 && (yy %100 != 0 || yy % 400 == 0)) return ans + 1;
         return ans;
     }
