505feead676c508359decf0285bda979b16ec7e4
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-01 23:27:45 -0500

    Time: 2 ms (72.59%), Space: 56.9 MB (97.07%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long []ps = new long[n];
+        ps[0] = chalk[0];
+        for (int i = 1; i<n; i++) {
+            ps[i] = ps[i-1] + chalk[i];
+        }
+        k = (int) (k % ps[n-1]);
+        int mid = 0;
+        int st = 0, en = n-1;
+        while (st < en) {
+            mid = st + (en - st)/2;
+            if (ps[mid] > k) {
+                en = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return en;
+    }
+}
