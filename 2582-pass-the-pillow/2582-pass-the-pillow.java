c8dd98ef8285d0210299926fce698c21fe45d80b
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-02 00:37:06 -0500

    Time: 0 ms (100.00%), Space: 40.2 MB (65.61%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -2,8 +2,7 @@ class Solution {
     public int passThePillow(int n, int time) {
         int nn = 2 * (n - 1);
         int tn = time % nn;
-        // if (tn == 0) return n;
         if (tn < n) return tn + 1;
-        return 2*(n-1) - tn + 1;
+        return nn - tn + 1;
     }
 }
