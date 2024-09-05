26149cd932440b76d78dabd8708f0a5d8605a478
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-05 01:01:11 -0500

    Time: 0 ms (100.00%), Space: 44.5 MB (96.39%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,43 +1,56 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 class Solution {
-    
-    String[] template;
-    
-    public List<List<String>> solveNQueens(int n) {
-        int r[] = new int[n], c[] = new int[n], d1[] = new int[2*n + 2], d2[] = new int[2*n + 2];
-        Set<List<String>> ans = new HashSet();
-        template = new String[n];
-        char[] line = new char[n];
-        Arrays.fill(line, '.');
-        for (int i = 0; i < n; i++) {
-          line[i] = 'Q';
-          template[i] = String.valueOf(line);
-          line[i] = '.';
-        }
-        rec(n, r, c, d1, d2, 0, new ArrayList(), ans);
-        return ans.stream().toList();
+  private int n;
+  private List<List<String>> builder;
+  private String[] template;
+  private String[] board;
+  public List<List<String>> solveNQueens(int n) {
+    return new AbstractList<List<String>>() {
+      private List<List<String>> cache = null;
+      @Override
+      public List<String> get(int index) {
+        if (cache == null) 
+        cache = solveNQueens1(n);
+        return cache.get(index);
+      }
+      @Override
+      public int size() {
+        if (cache == null) 
+        cache = solveNQueens1(n);
+        return cache.size();
+      }
+    };
+  }
+  public List<List<String>> solveNQueens1(int n) {
+    this.n = n;
+    this.builder = new ArrayList<>();
+    this.template = new String[n];
+    this.board = new String[n];
+    char[] line = new char[n];
+    Arrays.fill(line, '.');
+    for (int i = 0; i < n; i++) {
+      line[i] = 'Q';
+      template[i] = String.valueOf(line);
+      line[i] = '.';
     }
-    
-    private void rec(int n, int[] r, int[] c, int[] d,int[] d2, int i, List<String> cur, Set<List<String>> ans) {
-        if (cur.size() == n) {
-            ans.add(new ArrayList(cur));
-            return;
-        }
-        if (i == n) return;
-        // if (i == 3) System.out.println(Arrays.toString(d2));
-        for (int j = 0; j< n; j++) {
-            if (c[j] == 1 || d[i-j+n] == 1 || d2[i+j] == 1) continue;
-            c[j] = 1;
-            d[i-j+n] = 1;
-            d2[i+j] = 1;
-            int ind = cur.size();
-            String temp = template[j];
-            cur.add(temp);
-            rec(n, r, c, d, d2, i+1, cur, ans);
-            cur.remove(ind);
-            c[j] = 0;
-            d[i-j+n] = 0;
-            d2[i+j] = 0;
-        }
-        return;
+    enumerateRow(0, 0, 0, 0);
+    return builder;
+  }
+  private void enumerateRow(int i, int shadow0, int shadow1, int shadow2) {
+    int shadowSum = shadow0 | shadow1 | shadow2;
+    for (int j = 0; j < n; j++) {
+      int cell = 1 << j;
+      if ((shadowSum & cell) == 0) {
+        board[i] = template[j];
+        if (i == n - 1){
+            // System.out.printf("%s %s %s \n", Integer.toBinaryString((int)shadow0), Integer.toBinaryString((int)shadow1), Integer.toBinaryString((int)shadow2));
+            builder.add(new ArrayList<>(Arrays.asList(board)));
+        }else
+        enumerateRow(i + 1, (shadow0 | cell) << 1, shadow1 | cell, (shadow2 | cell) >> 1);
+      }
     }
+  }
 }
