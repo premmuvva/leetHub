0f4519e1741306e02806146f226b9d355a62c476
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-05 00:33:39 -0500

    Time: 9 ms (14.55%), Space: 44.8 MB (46.14%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,7 +1,14 @@
 class Solution {
+    
+    String[] template;
+    
     public List<List<String>> solveNQueens(int n) {
         int r[] = new int[n], c[] = new int[n], d1[] = new int[2*n + 2], d2[] = new int[2*n + 2];
         Set<List<String>> ans = new HashSet();
+        template = new String[n];
+        for (int i = 0; i<n; i++) {
+            template[i] = ".".repeat(i) + 'Q' + ".".repeat(n-i-1);
+        }
         rec(n, r, c, d1, d2, 0, new ArrayList(), ans);
         return ans.stream().toList();
     }
@@ -9,7 +16,6 @@ class Solution {
     private void rec(int n, int[] r, int[] c, int[] d,int[] d2, int i, List<String> cur, Set<List<String>> ans) {
         if (cur.size() == n) {
             ans.add(new ArrayList(cur));
-            // System.out.println(Arrays.toString(d2));
             return;
         }
         if (i == n) return;
@@ -20,7 +26,7 @@ class Solution {
             d[i-j+n] = 1;
             d2[i+j] = 1;
             int ind = cur.size();
-            String temp = ".".repeat(j) + 'Q' + ".".repeat(n-j-1);
+            String temp = template[j];
             cur.add(temp);
             rec(n, r, c, d, d2, i+1, cur, ans);
             cur.remove(ind);
