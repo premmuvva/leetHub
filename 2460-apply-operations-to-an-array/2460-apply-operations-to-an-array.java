ab9dcaa54c1f165439be487d1f0f68e54a49cc3a
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-07 02:25:12 -0500

    Time: 0 ms (100.00%), Space: 43.6 MB (63.43%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,23 +1,6 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int n = nums.length - 1, zc = 0, le = -1;
-        // Queue<Integer> qu = new LinkedList();
-        // for (int i = 0; i < n; i++) {
-        //     if (nums[i] == 0) {
-        //         qu.add(i);
-        //         zc++;
-        //         continue;
-        //     }
-        //     if (nums[i] == nums[i+1]) {
-        //         if (qu.isEmpty()) nums[i] *= 2;
-        //         else {
-        //             nums[qu.remove()] = nums[i] * 2;
-        //             qu.add(i);
-        //         }
-        //         qu.add(++i);
-        //         zc++;
-        //     }
-        // }
         for (int i = 0; i<n; i++) {
             if (nums[i] == 0) continue;
             if (nums[i] == nums[i+1]) {
@@ -30,9 +13,6 @@ class Solution {
         }
         if (nums[n] != 0 && le != n) nums[zc++] = nums[n];
         n++;
-        // for (int i = 0; i < n;i++) {
-        //     if (nums[i] != 0) nums[zc++] = nums[i];
-        // }
         for (int i = zc; i<n; i++) nums[i] = 0;
         return nums;
     }
