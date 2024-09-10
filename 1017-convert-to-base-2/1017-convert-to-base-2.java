74c60df7f45f7a3e045ef198a5bb3d94841ab2c5
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-10 02:17:51 -0500

    Time: 7 ms (6.05%), Space: 41.3 MB (13.06%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String baseNeg2(int n) {
+        
+        if (n == 0) return "0";
+        int rem = 0;
+        String ans = "";
+        while(n != 0) {
+            rem = n % 2;
+            if (n < 0) n--;
+            n = n / -2;
+            if (rem == -1 && n == 0) n = 1;
+            ans = Math.abs(rem) + ans;
+        }
+        return ans;
+    }
+}
