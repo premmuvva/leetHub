5c987a49597b4d22fdeaa622c534dd3532e0d41f
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-10 20:49:33 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (16.22%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int ans = 0;
-        start ^= goal;
+        start ^= goal; //reusing start for xor and goal for ans
+        goal = 0;
         while(start > 0) {
-            ans ++;
+            goal ++;
             start = (start - 1) & start;
         }
-        return ans;
+        return goal;
     }
 }
