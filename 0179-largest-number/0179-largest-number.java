3c87e7f0ea4c9dd2759782316c88003ba591da6e
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-09-18 01:09:14 -0700

    Time: 8 ms (20.96%), Space: 42.6 MB (97.23%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,13 +1,19 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String ans = Arrays.stream(nums)
-            // .peek(System.out::println)
-            .mapToObj(i -> String.valueOf(i))
-            .sorted((b, a) -> compareTo(a, b))
-            .collect(Collectors.joining());
+        // String ans = Arrays.stream(nums)
+        //     .mapToObj(i -> String.valueOf(i))
+        //     .sorted((b, a) -> compareTo(a, b))
+        //     .collect(Collectors.joining());
             // .reduce(new StringBuilder(), (acc, str) -> acc.append(str.charAt(0)), (sb1, sb2) -> sb1.append(sb2))
             // .toString();
             // .map(StringBuilder::toString);        
+        String ans = Arrays.stream(nums)
+            .mapToObj(i -> Integer.valueOf(i))
+            .sorted((b, a) -> compareTo(a, b))
+            .map(i -> String.valueOf(i))
+            // .peek(System.out::println)
+            // .collect(Collectors.joining());
+            .reduce("", String::concat);
         return ans.charAt(0) == '0' ? "0": ans;
     }
     
@@ -25,5 +31,22 @@ class Solution {
             }
         }
         return 1;
+    }
+    
+    int compareTo(int a, int b) {
+        long ta = a, tb = b;
+        int ac = 1, bc = 1;
+        while(ta > 0) {
+            ta/=10;
+            ac*=10;
+        }
+        while(tb > 0) {
+            tb/=10;
+            bc*=10;
+        }
+        // System.out.printf("%d %d %d %d\n", a, b, ac, bc);
+        ta = (long)a * bc + b;
+        tb = (long)b * ac + a;
+        return ta == tb ? (a > b ? 1 : -1) : (ta > tb ? 1 : -1);
     }
 }
