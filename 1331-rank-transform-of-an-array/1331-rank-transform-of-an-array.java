161d42ef7b0c668f033de844b54c2dc8520cce81
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-10-01 20:29:48 -0700

    Time: 54 ms (13.39%), Space: 69.9 MB (5.29%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -2,9 +2,9 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         Map<Integer, List<Integer>> hm = new TreeMap<Integer, List<Integer>>();
         for (int i = 0; i < arr.length; i++) {
-            List<Integer> li = hm.getOrDefault(arr[i], new ArrayList());
-            li.add(i+1);
-            hm.put(arr[i], li);
+            hm.computeIfAbsent(arr[i], k -> new ArrayList()).add(i+1);
+            // li.add(i+1);
+            // hm.put(arr[i], li);
         }
         int i = 1;
         for (Map.Entry<Integer, List<Integer>> entry: hm.entrySet()) {
