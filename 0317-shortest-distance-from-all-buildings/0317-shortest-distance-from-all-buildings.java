b1b425d51a6d6c3b9826dc3dbcfdd8456bd1552a
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-10-09 00:30:09 -0500

    0317-SHORTEST-DISTANCE-FROM-ALL-BUILDINGS Stats: Time: 107 ms (82.49%), Space: 44.7 MB (67.62%) - LeetHub

---
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int shortestDistance(int[][] grid) {
+        // Next four directions.
+        int dirs[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        // Total Mtrix to store total distance sum for each empty cell.
+        int[][] total = new int[rows][cols];
+
+        int emptyLandValue = 0;
+        int minDist = Integer.MAX_VALUE;
+
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+
+                // Start a BFS from each house.
+                if (grid[row][col] == 1) {
+                    minDist = Integer.MAX_VALUE;
+
+                    // Use a queue to perform a BFS, starting from the cell at (r, c).
+                    Queue<int[]> q = new LinkedList<>();
+                    q.offer(new int[]{ row, col });
+
+                    int steps = 0;
+
+                    while (!q.isEmpty()) {
+                        steps++;
+
+                        for (int level = q.size(); level > 0; --level) {
+                            int[] curr = q.poll();
+
+                            for (int[] dir : dirs) {
+                                int nextRow = curr[0] + dir[0];
+                                int nextCol = curr[1] + dir[1];
+
+                                // For each cell with the value equal to empty land value
+                                // add distance and decrement the cell value by 1.
+                                if (nextRow >= 0 && nextRow < rows &&
+                                    nextCol >= 0 && nextCol < cols &&
+                                    grid[nextRow][nextCol] == emptyLandValue) {
+                                    grid[nextRow][nextCol]--;
+                                    total[nextRow][nextCol] += steps;
+
+                                    q.offer(new int[]{ nextRow, nextCol });
+                                    minDist = Math.min(minDist, total[nextRow][nextCol]);
+                                }
+                            }
+                        }
+                    }
+
+                    // Decrement empty land value to be searched in next iteration.
+                    emptyLandValue--;
+                }
+            }
+        }
+
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # leetHub
 Collection of LeetCode questions to ace the coding interview! - Created using [LeetHub](https://github.com/QasimWani/LeetHub)
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Array
+|  |
+| ------- |
+| [0317-shortest-distance-from-all-buildings](https://github.com/premmuvva/leetHub/tree/master/0317-shortest-distance-from-all-buildings) |
+## Breadth-First Search
+|  |
+| ------- |
+| [0317-shortest-distance-from-all-buildings](https://github.com/premmuvva/leetHub/tree/master/0317-shortest-distance-from-all-buildings) |
+## Matrix
+|  |
+| ------- |
+| [0317-shortest-distance-from-all-buildings](https://github.com/premmuvva/leetHub/tree/master/0317-shortest-distance-from-all-buildings) |
+<!---LeetCode Topics End-->
