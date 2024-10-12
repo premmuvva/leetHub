0d0a9f3abe46a539ee78a918e12e882acfba2a21
PREM KUMAR REDDY MUVVA <premreddymuvva@gmail.com>
2024-10-11 22:56:21 -0500

    0252-MEETING-ROOMS Stats: Time: 5 ms (96.13%), Space: 45.9 MB (50.86%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         int n = intervals.length;
         Arrays.sort(intervals, (a, b) -> {
-            if (a[0] == b[0]) {
-                return b[1] - a[1];
-            }
+            // if (a[0] == b[0]) {
+            //     return b[1] - a[1];
+            // }
             return a[0] - b[0];
         });
         for (int i = 0 ;i <n-1; i++) {
